0be87aa45a27644f21eee36db7016b51614b2dda
lucaslopesx <lucas.lopesx1@gmail.com>
2024-10-28 22:00:02 -0300

    feat: day 9 part 1

---
+++ 2023/d9-mirage-maintenance/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+type History struct {
+	Sequences [][]int
+}
+
+func main() {
+	in := strings.Split(input, "\n")
+	res := runReports(in)
+	fmt.Print(res)
+}
+
+func runReports(in []string) int {
+	var histories []History
+	for _, line := range in {
+		history := processLine(line)
+		histories = append(histories, history)
+	}
+
+	sum := 0
+	for _, v := range histories {
+		sum += extrapolateHistory(v.Sequences)
+	}
+	return sum
+}
+
+func extrapolateHistory(sequences [][]int) int {
+	sequencesSize := len(sequences) - 1
+	lastValue := 0
+	for i := sequencesSize; i > 0; i-- {
+		current := sequences[i]
+		up := sequences[i-1]
+
+		currentValue := 0
+		if i != sequencesSize {
+			currentValue = current[len(current)-1]
+		}
+
+		upperValue := up[len(up)-1]
+
+		extrapolatedValue := upperValue + currentValue
+		sequences[i-1] = append(sequences[i-1], extrapolatedValue)
+		lastValue = extrapolatedValue
+	}
+
+	return lastValue
+}
+
+func processLine(line string) History {
+	var start, err = parseNumbers(line)
+	if err != nil {
+		return History{}
+	}
+	var diffs []int
+	var sequences [][]int
+	sequences = append(sequences, start)
+	currentValues := start
+	for {
+		isAllZeroes := true
+		for i := 0; i < len(currentValues)-1; i++ {
+			num1 := currentValues[i]
+			num2 := 0
+			if i != len(currentValues) {
+				num2 = currentValues[i+1]
+			}
+
+			diff := num2 - num1
+			if diff != 0 {
+				isAllZeroes = false
+			}
+
+			diffs = append(diffs, diff)
+		}
+
+		sequences = append(sequences, diffs)
+		if isAllZeroes {
+			break
+		}
+
+		currentValues = diffs
+		diffs = nil
+	}
+
+	return History{
+		Sequences: sequences,
+	}
+}
+
+func parseNumbers(input string) ([]int, error) {
+	numStrings := strings.Fields(input)
+	numbers := make([]int, 0, len(numStrings))
+	for _, numStr := range numStrings {
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			return nil, err
+		}
+		numbers = append(numbers, num)
+	}
+
+	return numbers, nil
+}
---
+++ 2023/d9-mirage-maintenance/solution_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestCalculateReports(t *testing.T) {
+	tests := []struct {
+		input    []string
+		expected int
+	}{
+		{
+			input: []string{
+				"0 3 6 9 12 15",
+				"1 3 6 10 15 21",
+				"10 13 16 21 30 45",
+			},
+			expected: 114,
+		},
+	}
+
+	for _, test := range tests {
+		result := runReports(test.input)
+
+		if result != test.expected {
+			t.Errorf("For input %v, expected %d, but got %d", test.input, test.expected, result)
+		}
+	}
+}
